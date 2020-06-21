7f03025caf5355085e20f02f67f79a25a59ebf03
Life Light <lifelightdev@gmail.com>
2020-06-21 19:16:56 +0200

    Ajout du test pour la recherche des IDs dans des URLs

--- src/main/java/Life/Light/RechercherIDLog/Find.java
+++ src/main/java/Life/Light/RechercherIDLog/Find.java
@@ -3,6 +3,7 @@ package Life.Light.RechercherIDLog;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Find {
@@ -10,5 +11,13 @@ public class Find {
         MultiValueMap<String, String> parameters = UriComponentsBuilder.fromHttpUrl(url).build().getQueryParams();
         List<String> id = parameters.get("id");
         return Integer.parseInt(id.get(0));
+    }
+
+    public List<Integer> iDsIn(List<String> urls) {
+        List<Integer> resultat = new ArrayList<>();
+        for (String url : urls){
+            resultat.add(iDIn(url));
+        }
+        return resultat;
     }
 }
--- src/test/java/Life/Light/RechercherIDLog/RechercherIdLogApplicationTests.java
+++ src/test/java/Life/Light/RechercherIDLog/RechercherIdLogApplicationTests.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class RechercherIdLogApplicationTests {
 
@@ -20,5 +23,20 @@ class RechercherIdLogApplicationTests {
 		Find find = new Find();
 		Assertions.assertThat(find.iDIn(url)).isEqualTo(835);
 	}
+
+	@Test
+	void find_IDs_in_2_URL() {
+		List<String> urls = new ArrayList<>();
+		urls.add("http://a.com?id=43");
+		urls.add("https://test.net?a=1&id=446&b=2&c=3");
+		Find find = new Find();
+		List<Integer> resultatObtenu = find.iDsIn(urls);
+		List<Integer> resultatAttendu = new ArrayList<>();
+		resultatAttendu.add(43);
+		resultatAttendu.add(446);
+		Assertions.assertThat(resultatObtenu).isEqualTo(resultatAttendu);
+	}
+
+
 
 }
