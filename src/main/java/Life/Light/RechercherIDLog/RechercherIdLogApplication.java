51d0e28664355f0e4c6f3b9abdc8e809b57e98a6
Life Light <lifelightdev@gmail.com>
2020-06-22 02:25:24 +0200

    Exécution du code

--- src/main/java/Life/Light/RechercherIDLog/RechercherIdLogApplication.java
+++ src/main/java/Life/Light/RechercherIDLog/RechercherIdLogApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RechercherIdLogApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RechercherIdLogApplication.class, args);
+		Find find = new Find();
+		find.printFirst();
+
 	}
 
 }
