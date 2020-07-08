5448bdf2a5e1ba62fe42314876c1ef4e1b825bc7
Life Light <lifelightdev@gmail.com>
2020-07-08 22:11:00 +0200

    Simplification de la méthode readDirectoryToURLs

--- src/main/java/Life/Light/RechercherIDLog/ReadFile.java
+++ src/main/java/Life/Light/RechercherIDLog/ReadFile.java
@@ -2,15 +2,7 @@ package Life.Light.RechercherIDLog;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class ReadFile {
     public List<String> readToURLs(File fichier) {
@@ -34,20 +26,10 @@ public class ReadFile {
     }
 
     public List<String> readDirectoryToURLs(String directory) {
-        List<String> resultat = new ArrayList<>();
-        try (Stream<Path> walk = Files.walk(Paths.get(directory))) {
 
-            List<String> result = walk.filter(Files::isRegularFile)
-                    .map(Path::toString).collect(Collectors.toList());
-            List<File> files = new ArrayList<>();
-            for (String fichier : result){
-                files.add(new File(fichier));
-            }
-            resultat.addAll(readAllToURLs(files));
+        File file = new File(directory);
+        List<File> listFiles = Arrays.asList(Objects.requireNonNull(file.listFiles()));
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return resultat;
+        return readAllToURLs(listFiles);
     }
 }
